25c1f27af8be9fe7cec7a837239417d0162e02bc
DLM4RI <mario.andres.castro.9.4@gmail.com>
2026-04-03 15:55:02 -0500

    Add logo-img component to login and register

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
+import LogoImg from '~/components/start/LogoImg.vue';
+
 definePageMeta({
   middleware: 'guest'
 })
 </script>
 
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-black">
+  <div class="min-h-screen w-full flex lg:flex-row flex-col lg:gap-16 lg:text-sm items-center justify-center bg-black">
+    <LogoImg class="mb-6" />
     <div class="p-8 rounded-2xl shadow-sm w-full max-w-md">
-      <h1 class="text-2xl font-semibold text-[#FFB800] mb-6 text-center">
+      <h1 class="text-2xl font-bold font-Viga text-white mb-6 text-center lg:text-left">
         Bienvenido de vuelta
       </h1>
       <LoginForm />
--- app/pages/auth/register.vue
+++ app/pages/auth/register.vue
@@ -5,9 +5,10 @@ definePageMeta({
 </script>
 
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-black">
+  <div class="min-h-screen w-full flex lg:flex-row flex-col lg:gap-16 lg:text-sm items-center justify-center bg-black">
+    <startLogoImg class="mb-6" />
     <div class="p-8 rounded-2xl shadow-sm w-full max-w-md">
-      <h1 class="text-2xl font-semibold text-white mb-6 text-center">
+      <h1 class="text-2xl font-bold font-Viga text-white mb-6 text-center lg:text-left">
         Crear cuenta
       </h1>
       <RegisterForm />
