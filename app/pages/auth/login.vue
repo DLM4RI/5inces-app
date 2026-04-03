5194bb5ae70cae0fc6946aca60e629b6a601960a
DLM4RI <mario.andres.castro.9.4@gmail.com>
2026-04-03 16:00:05 -0500

    Add logo-img component to login and register

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -8,7 +8,7 @@ definePageMeta({
 
 <template>
   <div class="min-h-screen w-full flex lg:flex-row flex-col lg:gap-16 lg:text-sm items-center justify-center bg-black">
-    <LogoImg class="mb-6" />
+    <LogoImg class="mb-6" /> 
     <div class="p-8 rounded-2xl shadow-sm w-full max-w-md">
       <h1 class="text-2xl font-bold font-Viga text-white mb-6 text-center lg:text-left">
         Bienvenido de vuelta
